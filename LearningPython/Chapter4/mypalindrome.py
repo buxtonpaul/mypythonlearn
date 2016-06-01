fa808648bbe5df7361d683b9ba4b65084de55796
Paul Buxton <Paul Buxton>
2016-06-01 18:56:09 +0100

    Adding new files

---
+++ LearningPython/Chapter4/mypalindrome.py
@@ -0,0 +1,6 @@
+
+source = 'level'
+seperated=list(source)
+seperated.reverse()
+revstring=''.join(seperated)
+print revstring == source
---
+++ LearningPython/Chapter4/regexpr.py
@@ -0,0 +1,11 @@
+import re
+
+target='Hello python world'
+match=re.match('Hello[ \t]*(.*)world',target)
+print match.group(1)
+
+target2='/home/user/paulbuxton'
+res2=re.split('[/:]',target2)
+folder=res2[-1] # last item in the list (indexing -1)
+path='/'.join(res2[:-1]) # everything upto the last item in the list
+print '%s is the folder %s is the path' % (folder, path)
