f3abeff519338249494de54e36d836e16920f18b
paulbuxton <paulbuxton@paulbuxton-VirtualBox.kl.imgtec.org>
2016-06-02 13:45:56 +0100

    Added comment too!

--- LearningPython/Chapter4/regexpr.py
+++ LearningPython/Chapter4/regexpr.py
@@ -8,4 +8,4 @@ target2='/home/user/paulbuxton'
 res2=re.split('[/:]',target2)
 folder=res2[-1] # last item in the list (indexing -1)
 path='/'.join(res2[:-1]) # everything upto the last item in the list
-print '%(folder)s is the folder %(path)s is the path' % vars()
+print '%(folder)s is the folder %(path)s is the path' % vars() # example using the template substitution to print variables
