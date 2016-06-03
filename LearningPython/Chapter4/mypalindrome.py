6843c5aa9709aa1319c3cf427f8b860de24efddf
paulbuxton <paulbuxton@paulbuxton-VirtualBox.kl.imgtec.org>
2016-06-03 14:16:30 +0100

    Imlemented palindrome sample

--- LearningPython/Chapter4/mypalindrome.py
+++ LearningPython/Chapter4/mypalindrome.py
@@ -1,10 +1,30 @@
 
-source = 'level'
-seperated=list(source)
-seperated.reverse()
-revstring=''.join(seperated)
-print revstring == source
 
 
-#alternative using slicing and stride
-print(source==source[::-1])
+
+def palindrome1(source):
+    """
+    Function to return if a string is a palinfrome or not
+    using list/string slicing
+    """
+    return source == source[::-1]
+
+
+def palindrome2(source):
+    """
+    Function to return is a string is a palidrome using list reversal
+    """
+    seperated = list(source) # seperate the string to a list
+    seperated.reverse()     # reverse the list
+    revstring = ''.join(seperated)   #convert to a string again
+    return  revstring == source
+
+testcases = ['abba', 'level', '101', 'fish', 'cheese']
+
+for test in testcases:
+    if palindrome1(test) == True:
+        print '%s is a Palindrome' % test
+    else:
+        print '%s is not a Palindrome' % test
+
+
