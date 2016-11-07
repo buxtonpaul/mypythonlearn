e0874ef9320112007dcc0fffd4675ec6672a4d00
Paul Buxton <Paul Buxton>
2016-11-07 08:13:31 +0000

    Updated with helpful comments

--- EdX Excercices/Module2/assignment5.py
+++ EdX Excercices/Module2/assignment5.py
@@ -2,12 +2,24 @@ import pandas as pd
 import numpy as np
 
 
+# Age, is considered continuous although could be ordinal
+# education is ordinal,
+# gain is continuous
+# race is nominal
+# capital loss is continuous
+# hours per week is continuous
+# sex is nominal
+# classification is ordinal
+
+
+
 #
 # TODO:
 # Load up the dataset, setting correct header labels.
 #
 # .. your code here ..
-
+# provide a column name for the indices in the data set to make the column easy to drop.
+# possible there is a way to drop the column whilst readin....
 censusdata=pd.read_csv('census.data',names=['indexnum','education', 'age', 'capital-gain', 'race', 'capital-loss', 'hours-per-week', 'sex', 'classification'],na_values=['?'])
 censusdata=censusdata.drop(labels=['indexnum'],axis=1)
 
@@ -28,6 +40,8 @@ censusdata=censusdata.drop(labels=['indexnum'],axis=1)
 ordered_education=['Preschool','1st-4th','5th-6th','7th-8th','9th','10th','11th','12th','HS-grad','Some-college','Bachelors','Masters','Doctorate']
 
 censusdata.education=censusdata.education.astype("category",ordered=True,categories=ordered_education).cat.codes
+# astype....cat.codes returns the ordered values of the series, which we then use to replace the original series.
+
 censusdata.classification=censusdata.classification.astype("category",ordered=True,categories=['<=50K','>50K']).cat.codes
 
 
@@ -45,7 +59,8 @@ censusdata.classification=censusdata.classification.astype("category",ordered=Tr
 # .. your code here ..
 
 newdata=pd.get_dummies(censusdata,columns=['race','sex'])
-
+# get_dummies expands out the race and sex columns to provide the binary classiciations for the
+# categories that exist. eg. sex_Male, race_Other...
 
 
 #
