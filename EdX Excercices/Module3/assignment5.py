b6bdd37a2fe6d7652128869bec6628c40b909823
Paul Buxton <Paul Buxton>
2016-11-09 12:23:38 +0000

    Updated again

--- EdX Excercices/Module3/assignment5.py
+++ EdX Excercices/Module3/assignment5.py
@@ -1,4 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+from pandas.tools.plotting import andrews_curves
+
+# Look pretty...
+matplotlib.style.use('ggplot')
+
+
+seeds = pd.read_csv("Datasets/wheat.data", index_col=0)
+
+
+
+
+
+#seeds=seeds.drop(labels=['area','perimeter'],axis=1)
+
 #
-# This code is intentionally missing!
-# Read the directions on the course lab page!
-#
+# TODO: Plot a parallel coordinates chart grouped by
+# the 'wheat_type' feature. Be sure to set the optional
+# display parameter alpha to 0.4
+# 
+# .. your code here ..
+
+plt.figure(dpi=267)
+andrews_curves(seeds, 'wheat_type',alpha=4)
+
+plt.show()
+
+
