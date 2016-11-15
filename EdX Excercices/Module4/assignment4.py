9e3393c63dd4fa150918c56137609dde160623f6
Paul Buxton <paul.buxton@kl.imgtec.org>
2016-11-15 13:38:43 +0000

    Assignment 4

--- EdX Excercices/Module4/assignment4.py
+++ EdX Excercices/Module4/assignment4.py
@@ -6,6 +6,8 @@ import random, math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
+from sklearn.decomposition import PCA
+
 def Plot2D(T, title, x, y, num_to_plot=40):
   # This method picks a bunch of random samples (images in your case)
   # to plot onto the chart:
@@ -26,7 +28,36 @@ def Plot2D(T, title, x, y, num_to_plot=40):
   # It also plots the full scatter:
   ax.scatter(T[:,x],T[:,y], marker='.',alpha=0.7)
 
+def Plot3D(T, title, x, y,z, num_to_plot=40):
 
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  ax.set_xlabel('Component: {0}'.format(x))
+  ax.set_ylabel('Component: {0}'.format(y))
+  ax.set_zlabel('Component: {0}'.format(z))
+
+
+  x_size = (max(T[:,x]) - min(T[:,x])) * 0.08
+  y_size = (max(T[:,y]) - min(T[:,y])) * 0.08
+  z_size = (max(T[:,z]) - min(T[:,z])) * 0.08
+
+  for i in range(num_to_plot):
+    img_num = int(random.random() * num_images)
+    x0, y0,z0 = T[img_num,x]-x_size/2., T[img_num,y]-y_size/2.,T[img_num,z]
+    x1, y1,z1 = T[img_num,x]+x_size/2., T[img_num,y]+y_size/2.,T[img_num,z]
+    
+    img = df.iloc[img_num,:].reshape(num_pixels, num_pixels)
+    ax.imshow(img, aspect='auto', cmap=plt.cm.gray, interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
+
+
+
+  ax.scatter(T[:,x],T[:,y],T[:,z], c='green', marker='.')
+
+  plt.show()
+  
+  
+
+  
 
 # A .MAT file is a .MATLAB file. The faces dataset could have came
 # in through .png images, but we'll show you how to do that in
@@ -53,6 +84,12 @@ for i in range(num_images):
 #
 # .. your code here ..
 
+pca=PCA(n_components=2)
+pca.fit(df)
+T=pca.transform(df)      
+
+Plot2D(T, "PCA plot of face_data reduced to 2 dimensions", 0, 1, num_to_plot=40)
+#Plot3D(df, "3d plot of dataframe", 0, 1,2, num_to_plot=40)
 
 #
 # TODO: Implement Isomap here. Reduce the dataframe df down
@@ -60,6 +97,14 @@ for i in range(num_images):
 # the first two components.
 #
 # .. your code here ..
+from sklearn import manifold
+iso=manifold.Isomap(n_neighbors=3, n_components=3)
+iso.fit(df)
+
+T=iso.transform(df)
+Plot2D(T, "ISPMap plot of face_data reduced to 3 dimensions (plotting first 2)", 0, 1, num_to_plot=40)
+Plot2D(T, "ISPMap plot of face_data reduced to 3 dimensions (plotting second 2)", 1, 2, num_to_plot=40)
+
 
 
 #
@@ -67,6 +112,6 @@ for i in range(num_images):
 # Even if you're not, just do it anyway.
 #
 # .. your code here ..
+#Plot
 
-
-plt.show()
+Plot3D(T,"3d Plot of Isomapped data",0,1,2,num_to_plot=40)
